46ce5e7840919b6e85e9317ebb6bd1ce447df995
Omar Ramirez <ramirez000245@gmail.com>
2020-06-18 14:20:37 -0700

    Now includes html reading

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1'
 const port = 3000;
 
-const server = http.createServer((req,res) => 
-{
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello World!');
+fs.readFile('index.html', (err, html) => {
+    if(err) {
+        throw err; 
+    }
+    
+    const server = http.createServer((req,res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/html'); // 'text/html' parses the html
+        res.write(html);
+        res.end();
+    });
+
+    server.listen(port, hostname, () => {
+        console.log('Server started on port ' + port);
+    });
 });
 
-server.listen(port, hostname, () =>
-{
-    console.log('Server started on port' + port);
-});
+
+
+
     
 
+/*
+NOTE: To restart server Ctrl + C and then input 'node app'
+
+*/
